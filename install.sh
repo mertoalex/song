5e2880a8f18fb3b9891da27c2c05c2456f2a002b
mertoalex <61164301+mertoalex@users.noreply.github.com>
2021-10-13 19:50:51 +0300

    E-dit

--- install.sh
+++ install.sh
@@ -4,23 +4,23 @@ read -p "do you want install song command ? (y/N) " install
 case $install in  
 	y|Y) eval '
 chmod +x song
-cat << EOFF
+echo "
 normal linux or termux? 
 (*1:linux
   2:termux)
-EOFF
+$"
 read lin
 case $lin in
 	2) echo you need manual move "song" file to bin ;;
 	*) sudo mv song /bin/ ;;
 esac
-cat << EOFF
+echo "
 this program need ffmpeg and youtube-dl
 we need your base distro (if another distro we can not install this auto)
 ( 1: Arch
   2: Debian
  *3: manual install)
-EOFF
+$"
 read deb
 case $deb in
 	1) sudo pacman -Syyu youtube-dl ffmpeg ;;
